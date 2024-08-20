1a11f6d4f093afffece76527de7973a9df5c3526
Aladser <Aladser@users.noreply.github.com>
2024-08-20 15:03:08 +0900

    обновлены url's

--- authen_drf/urls.py
+++ authen_drf/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
+from authen_drf.apps import AuthenDrfConfig
 from authen_drf.views import UserListAPIView, UserUpdateAPIView, UserRetrieveAPIView
 
+app_name = AuthenDrfConfig.name
+
 urlpatterns = [
-    path('', UserListAPIView.as_view(), name='user_list'),
-    path('<int:pk>', UserRetrieveAPIView.as_view(), name='user_detal'),
-    path('<int:pk>/update', UserUpdateAPIView.as_view(), name='user_update'),
+    path('', UserListAPIView.as_view(), name='list'),
+    path('<int:pk>', UserRetrieveAPIView.as_view(), name='detal'),
+    path('<int:pk>/update', UserUpdateAPIView.as_view(), name='update'),
 ]
--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('authen_drf.urls')),
+    path('user/', include('authen_drf.urls', namespace='user')),
     path('', include('lms.urls', namespace='lms'))
 ]
