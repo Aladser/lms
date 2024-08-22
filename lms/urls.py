6741c5a14df80f847c472f944a4a2c373a63b4f9
Aladser <Aladser@users.noreply.github.com>
2024-08-22 23:40:34 +0900

    добавлены фильтрация по дате

--- lms/urls.py
+++ lms/urls.py
@@ -1,8 +1,6 @@
-from django.conf.urls.static import static
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from config.settings import MEDIA_URL, MEDIA_ROOT
 from lms.apps import LearningConfig
 from lms.views import *
 
@@ -17,4 +15,4 @@ urlpatterns = [
     path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
     path('lesson/<int:pk>/update', LessonUpdateAPIView.as_view(), name='lesson_update'),
     path('lesson/<int:pk>/delete', LessonDestroyAPIView.as_view(), name='lesson_delete'),
-] + router.urls  + static(MEDIA_URL, document_root=MEDIA_ROOT)
+] + router.urls
--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics
+from rest_framework.filters import SearchFilter
 
-# Create your views here.
+from payment.models import Payment
+from payment.serializers import PaymentSerializer
+
+
+# --- ПЛАТЕЖ ---
+class PaymentListAPIView(generics.ListAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['date']
