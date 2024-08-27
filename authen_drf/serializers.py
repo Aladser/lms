30ed3d7d35b34e385d133a2710424e5b24ca878f
Aladser <Aladser@users.noreply.github.com>
2024-08-27 17:48:09 +0900

    обновлен показ данных пользователя

--- authen_drf/serializers.py
+++ authen_drf/serializers.py
@@ -4,9 +4,13 @@ from authen_drf.models import User
 from payment.serializers import PaymentSerializer
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserDetailSerializer(serializers.ModelSerializer):
     payment = PaymentSerializer(many=True, source='payments', read_only=True)
-
     class Meta:
         model = User
         fields = ('id', 'email', 'first_name', 'last_name', 'payment', 'is_staff', 'is_active', 'groups')
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'first_name', 'last_name')
--- authen_drf/views.py
+++ authen_drf/views.py
@@ -2,19 +2,29 @@ from rest_framework import generics
 from rest_framework.permissions import AllowAny
 
 from authen_drf.models import User
-from authen_drf.serializers import UserSerializer
+from authen_drf.permissions import IsPersonalProfilePermission
+from authen_drf.serializers import  UserListSerializer, UserDetailSerializer
+
 
 # --- Пользователь ---
+# LIST
 class UserListAPIView(generics.ListAPIView):
-    serializer_class = UserSerializer
+    serializer_class = UserListSerializer
     queryset = User.objects.all()
 
+# RETRIEVE
 class UserRetrieveAPIView(generics.RetrieveAPIView):
-    serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    def get_serializer_class(self):
+        user_obj_pk = self.request.__dict__['parser_context']['kwargs']['pk']
+        if user_obj_pk == self.request.user.pk:
+            return UserDetailSerializer
+        else:
+            return UserListSerializer
 
 class UserCreateAPIView(generics.CreateAPIView):
-    serializer_class = UserSerializer
+    serializer_class = UserDetailSerializer
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
 
@@ -24,9 +34,11 @@ class UserCreateAPIView(generics.CreateAPIView):
         user.save()
 
 class UserUpdateAPIView(generics.UpdateAPIView):
-    serializer_class = UserSerializer
+    serializer_class = UserDetailSerializer
+
     queryset = User.objects.all()
+    permission_classes = (IsPersonalProfilePermission,)
 
 class UserDestroyAPIView(generics.DestroyAPIView):
-    serializer_class = UserSerializer
+    serializer_class = UserDetailSerializer
     queryset = User.objects.all()
