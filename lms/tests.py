22912f798c6110ddfb6e87760a9e398e8d4eb3ca
Aladser <Aladser@users.noreply.github.com>
2024-09-04 09:53:56 +0900

    обновлены тесты

--- lms/tests.py
+++ lms/tests.py
@@ -1,5 +1,4 @@
 from http.client import responses
-
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -11,7 +10,7 @@ user_params = {'email': 'admin@test.ru','first_name': 'Админ','last_name': 
 course_pk = 10
 lesson_pk = 10
 course_params = {'pk':course_pk, 'name': 'математика'}
-lesson_params = {'pk': lesson_pk, "name": "test", "description": " описание test"}
+lesson_params = {'pk': lesson_pk, "name": "степени", "description": "возведение в степень"}
 
 class LessonTestCase(APITestCase):
     def setUp(self):
@@ -23,8 +22,10 @@ class LessonTestCase(APITestCase):
 
     def test_list(self):
         url = reverse('lms:lesson_list')
+
         response = self.client.get(url)
         received_lessons = response.json()
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(received_lessons['count'], 1)
 
@@ -32,17 +33,37 @@ class LessonTestCase(APITestCase):
         url = reverse('lms:lesson_retrieve', kwargs={'pk':lesson_pk})
         response = self.client.get(url)
         received_lesson = response.json()
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(received_lesson['name'], self.lesson.name)
 
     def test_create(self):
         url = reverse('lms:lesson_create')
-        data = {
-            "name": "test2",
-            "description":"описание test2",
+        sent_lesson = {
+            "name": "уравнения",
+            "description":"что такое уравнения",
             "course":course_pk
         }
 
-        response = self.client.post(url, data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.client.post(url, sent_lesson)
+        received_lesson = response.json()
 
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(received_lesson['name'], sent_lesson['name'])
+
+    def test_update(self):
+        url = reverse('lms:lesson_update', kwargs={'pk':lesson_pk})
+        sent_lesson = {'name': 'степени: продолжение','description': 'решение примеров'}
+
+        response  = self.client.patch(url, sent_lesson)
+        received_lesson = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(received_lesson['name'], sent_lesson['name'])
+        self.assertEqual(received_lesson['description'], sent_lesson['description'])
+
+    def test_delete(self):
+        url = reverse('lms:lesson_delete', kwargs={'pk': lesson_pk})
+        response  = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Lesson.objects.all().count(), 0)
