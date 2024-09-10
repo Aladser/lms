44d69f9080b6f73c3c9de8f2d418d33c6da8e044
Aladser <Aladser@users.noreply.github.com>
2024-09-10 16:34:03 +0900

    обновление проверка активности пользователей

--- config/settings.py
+++ config/settings.py
@@ -146,7 +146,7 @@ CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
 CELERY_BEAT_SCHEDULE = {
     'check_user_activities': {
-        'task': 'lms.tasks.check_user_activities',
+        'task': 'authen_drf.tasks.check_user_activities',
         'schedule': timedelta(seconds = 10),
         'start_time': datetime.now(pytz.timezone(TIME_ZONE))
     },
--- lms/tasks.py
+++ lms/tasks.py
@@ -1,11 +1,6 @@
-from datetime import datetime
-
-import pytz
 from celery import shared_task
 from django.core.mail import send_mail
 
-from authen_drf.models import User
-from config import settings
 from config.settings import EMAIL_HOST_USER
 from lms.models import Course, UserSubscription
 
@@ -30,20 +25,4 @@ def send_course_updating_notification(course_id):
     response = send_mail(subject, message, EMAIL_HOST_USER, email_list)
     return f"Отправлены почтовые уведомления на обновление курса {course}" if response == 1 else response
 
-
-@shared_task
-def check_user_activities():
-    """Периодическая задача проверки активности пользователя"""
-
-    info = []
-
-    datetime_now = datetime.now(pytz.timezone(settings.TIME_ZONE))
-    for user in User.objects.all():
-        if user.last_login is None:
-            continue
-
-        lastlogin_interval = (datetime_now - user.last_login).days
-        info.append(lastlogin_interval)
-
-    return info
 
